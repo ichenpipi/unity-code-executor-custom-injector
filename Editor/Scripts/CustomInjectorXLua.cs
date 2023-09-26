f90e982b9190bb25dbf082fa61f719af5082b8c9
陈皮皮 <952157129@qq.com>
2023-09-26 12:58:50 +0800

    First commit

---
+++ Editor/Scripts/CustomInjectorXLua.cs
@@ -0,0 +1,55 @@
+﻿using ChenPipi.CodeExecutor.Editor;
+using ChenPipi.CodeExecutor.Examples;
+using UnityEditor;
+using UnityEngine;
+
+namespace ChenPipi.CodeExecutorCustomInjector.Editor
+{
+
+    /// <summary>
+    /// 给 CodeExecutor 注入代码执行模式
+    /// </summary>
+    public static class CustomInjectorXLua
+    {
+
+        /// <summary>
+        /// xLua 所在的程序集名称，置空则使用默认程序集 "Assembly-CSharp"
+        /// </summary>
+        public const string XLuaAssemblyName = "Assembly-CSharp";
+
+        /// <summary>
+        /// 注册 CodeExecutor 执行模式
+        /// </summary>
+        [CodeExecutorRegistration(1)]
+        private static void Register()
+        {
+            // 禁用内置的 xLua 执行模式
+            if (CodeExecutorManager.enableBuiltinExecModeXLua)
+            {
+                CodeExecutorManager.enableBuiltinExecModeXLua = false;
+            }
+            // 初始化 xLua Helper
+            if (!ExecutionHelperXLua.isReady && !ExecutionHelperXLua.Init(XLuaAssemblyName))
+            {
+                return;
+            }
+            // 注册
+            CodeExecutorManager.RegisterExecMode(new ExecutionMode
+            {
+                name = "xLua (Standalone)",
+                executor = Executor,
+            });
+        }
+
+        private static object[] Executor(string code)
+        {
+            object[] results = ExecutionHelperXLua.ExecuteCode(code);
+
+            CodeExecutorManager.ShowNotification("已执行");
+
+            return results;
+        }
+
+    }
+
+}
---
+++ Editor/Scripts/CustomInjectorXLuaInGame.cs
@@ -0,0 +1,65 @@
+﻿using ChenPipi.CodeExecutor.Editor;
+using ChenPipi.CodeExecutor.Examples;
+using UnityEditor;
+using UnityEngine;
+
+namespace ChenPipi.CodeExecutorCustomInjector.Editor
+{
+
+    /// <summary>
+    /// 给 CodeExecutor 注入代码执行模式
+    /// </summary>
+    public static class CustomInjectorXLuaInGame
+    {
+
+        /// <summary>
+        /// 注册 CodeExecutor 执行模式
+        /// </summary>
+        [CodeExecutorRegistration(2)]
+        private static void Register()
+        {
+            // 初始化 xLua Helper
+            if (!ExecutionHelperXLua.isReady && !ExecutionHelperXLua.Init(CustomInjectorXLua.XLuaAssemblyName))
+            {
+                return;
+            }
+            // 注册
+            CodeExecutorManager.RegisterExecMode(new ExecutionMode
+            {
+                name = "xLua (InGame)",
+                executor = Executor,
+            });
+        }
+
+        private static object[] Executor(string code)
+        {
+            object luaEnv = GetLuaEnv();
+            if (luaEnv == null)
+            {
+                if (!CodeExecutorManager.ShowNotification("请先运行游戏"))
+                {
+                    EditorUtility.DisplayDialog("Code Executor", "请先运行游戏！", "OK");
+                }
+                return null;
+            }
+
+            object[] results = ExecutionHelperXLua.ExecuteCode(code, luaEnv);
+
+            CodeExecutorManager.ShowNotification("已执行");
+
+            return results;
+        }
+
+        private static object GetLuaEnv()
+        {
+            // Return your LuaEnv object
+            if (LuaManager.Instance != null)
+            {
+                return LuaManager.Instance.luaEnv;
+            }
+            return null;
+        }
+
+    }
+
+}
